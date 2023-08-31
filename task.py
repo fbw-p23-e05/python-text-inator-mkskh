2ffadbcdb0efd59ae709fccb207e19dd097346c0
mkskh <maksym.kharchenko@dci-student.org>
2023-09-01 00:54:33 +0200

    made code more easier

--- task.py
+++ task.py
@@ -3,24 +3,29 @@
 #At the start I tried to write just only with if else where I just compare last letter with list_con. But this way is not working. Or maybe it require some specific syntax ?
 
 
-list_con = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
+# list_con = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
 list_vow = ['a', 'e', 'i', 'o', 'u']
 
-text = str(input('Enter the word: '))
-last_letter = text[-1]
+text = input('Enter the word: ')
+# last_letter = text[-1]
+
+if text[-1] in list_vow:
+    print(text + '-inator', str(len(text)) + '000')
+else:
+    print(text + 'inator', str(len(text)) + '000')
 
 
-for letter in list_con:
-    if last_letter != letter:
-        pass
-    elif last_letter == letter:
-        print(text + 'inator', str(len(text)) + '000')
+# for letter in list_con:
+#     if last_letter != letter:
+#         pass
+#     elif last_letter == letter:
+#         print(text + 'inator', str(len(text)) + '000')
 
-for letter in list_vow:
-    if last_letter != letter:
-        pass
-    elif last_letter == letter:
-        print(text + '-inator', str(len(text)) + '000')
+# for letter in list_vow:
+#     if last_letter != letter:
+#         pass
+#     elif last_letter == letter:
+#         print(text + '-inator', str(len(text)) + '000')
 
 
 
