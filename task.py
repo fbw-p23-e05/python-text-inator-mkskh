6f50797a3c9d36239d8a485b60cf43bcb28b34c8
mkskh <maksym.kharchenko@dci-student.org>
2023-09-01 00:57:24 +0200

    added changes

--- task.py
+++ task.py
@@ -2,18 +2,24 @@
 
 #At the start I tried to write just only with if else where I just compare last letter with list_con. But this way is not working. Or maybe it require some specific syntax ?
 
-
+# ------- from first try -------
 # list_con = [ 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' ]
 list_vow = ['a', 'e', 'i', 'o', 'u']
 
 text = input('Enter the word: ')
-# last_letter = text[-1]
+# last_letter = text[-1] ------- from first try -------
+
+# ------- second try -------
 
 if text[-1] in list_vow:
     print(text + '-inator', str(len(text)) + '000')
 else:
     print(text + 'inator', str(len(text)) + '000')
 
+# ----------------------------
+
+
+# ------- first try -------
 
 # for letter in list_con:
 #     if last_letter != letter:
